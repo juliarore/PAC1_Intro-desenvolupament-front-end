d8206ebeb7bf0bc26a5eabbbfd7032e24f1dcd2b
Júlia Romero <juliarore6@gmail.com>
2025-10-02 12:24:03 +0200

    refactor: email validation

--- PAC1_Ex2_1/script.js
+++ PAC1_Ex2_1/script.js
@@ -19,9 +19,14 @@ function showSuccess(input) {
 }
 
 // Check email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    
+    if (re.test(input.value.trim())) {
+        showSuccess(input.value);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
 // Check required fields
@@ -58,5 +63,5 @@ form.addEventListener('submit', e => {
     checkRequired([username, email, password, password2]);
     checkLength(username, 3, 15);
     checkLength(password, 6, 25);
-
+    checkEmail(email);
 });  
