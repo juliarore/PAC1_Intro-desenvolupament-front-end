d6f0265e770653fe28d2d437f271ccca9cc90070
Júlia Romero <juliarore6@gmail.com>
2025-10-04 00:54:02 +0200

    feat: add password hint on focus

--- PAC1_Ex2_1/script.js
+++ PAC1_Ex2_1/script.js
@@ -1,87 +1,101 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const password2 = document.getElementById('password-confirmation');
+    const passwordHint = document.getElementById('password-hint');
 
-// Show input error message
-function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-}
-
-// Show success outline
-function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-}
-
-// Check email is valid
-function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
-    if (re.test(input.value.trim())) {
-        showSuccess(input.value);
-    } else {
-        showError(input, 'Email is not valid');
+    // Show input error message
+    function showError(input, message) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control error';
+        const small = formControl.querySelector('.error-message');
+        small.innerText = message;
     }
-}
 
-// Check required fields
-function checkRequired(inputArr) {
-    inputArr.forEach(function(input) {
-        if(input.value.trim() === '') { 
-            showError(input, `${getFieldName(input)} is required`); 
+    // Show success outline
+    function showSuccess(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+    }
+
+    // Show password hint
+    function showPasswordHint() {
+        passwordHint.className = 'password-hint visible';
+    }
+
+    // Hide password hint
+    function hidePasswordHint() {
+        passwordHint.className = 'password-hint';
+    }
+
+    // Check email is valid
+    function checkEmail(input) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if (re.test(input.value.trim())) {
+            showSuccess(input.value);
+        } else {
+            showError(input, 'Email is not valid');
+        }
+    }
+
+    // Check required fields
+    function checkRequired(inputArr) {
+        inputArr.forEach(function(input) {
+            if(input.value.trim() === '') { 
+                showError(input, `${getFieldName(input)} is required`); 
+            } else {
+                showSuccess(input);
+            }
+        });
+    }    
+
+    // Check input length
+    function checkLength(input, min, max) {
+        if (input.value.length < min) {
+            showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+        }  else if (input.value.length > max) { 
+            showError(input, `${getFieldName(input)} must be less than ${max} characters`);
         } else {
             showSuccess(input);
         }
-    });
-}    
+    }            
 
-// Check input length
-function checkLength(input, min, max) {
-    if (input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-    }  else if (input.value.length > max) { 
-        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
-    } else {
-        showSuccess(input);
+    // Check password is valid
+    function checkPassword (input) {
+        const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`~!@#$%^&*()_\-+={}\[\]|:;"'<>,.?/]).+$/;
+        
+        if (re.test(input.value.trim())) {
+            showSuccess(input.value);
+        } else {
+            showError(input, 'Password is not valid');
+        }
     }
-}            
 
-// Check password is valid
-function checkPassword (input) {
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`~!@#$%^&*()_\-+={}\[\]|:;"'<>,.?/]).+$/;
-    
-    if (re.test(input.value.trim())) {
-        showSuccess(input.value);
-    } else {
-        showError(input, 'Password must include at least one uppercase letter, one lowercase letter, one number and one special character');
+    // Check passwords match
+    function checkPasswordsMatch(input1, input2) {
+        if (input1.value !== input2.value) {
+            showError(input2, 'Passwords do not match');
+        }
     }
-}
 
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
-    if (input1.value !== input2.value) {
-        showError(input2, 'Passwords do not match');
+    // Get fieldname
+    function getFieldName(input) {
+        return input.id.charAt(0).toUpperCase() + input.id.slice(1).replace(/[-_]/g, ' ');
     }
-}
 
-// Get fieldname
-function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
+    // Event listeners
+    form.addEventListener('submit', e => {
+        e.preventDefault();
 
-// Event listeners
-form.addEventListener('submit', e => {
-    e.preventDefault();
+        checkRequired([username, email, password, password2]);
+        checkLength(username, 3, 15);
+        checkLength(password, 8, 25);
+        checkEmail(email);
+        checkPassword(password);
+        checkPasswordsMatch(password, password2);
+    });  
 
-    checkRequired([username, email, password, password2]);
-    checkLength(username, 3, 15);
-    checkLength(password, 8, 25);
-    checkEmail(email);
-    checkPassword(password);
-    checkPasswordsMatch(password, password2);
-});  
+    password.addEventListener('focus', showPasswordHint);
+    password.addEventListener('blur', hidePasswordHint);
