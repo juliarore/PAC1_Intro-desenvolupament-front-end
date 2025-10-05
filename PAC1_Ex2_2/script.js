d02ed709584c0a1680a001b421b7d5571d93fedc
Júlia Romero <juliarore6@gmail.com>
2025-10-05 13:37:08 +0200

    feat: implement exchange rate calculation and event listeners

---
+++ PAC1_Ex2_2/script.js
@@ -0,0 +1,33 @@
+const currencyElement_one = document.getElementById('currency-one');
+const amountElement_one = document.getElementById('amount-one');
+const currencyElement_two = document.getElementById('currency-two');
+const amountElement_two = document.getElementById('amount-two');
+
+const rateElement = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+    const currency_one = currencyElement_one.value;
+    const currency_two = currencyElement_two.value;
+
+    fetch(`https://open.er-api.com/v6/latest/${currency_one}`)        
+    .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            const rate = data.rates[currency_two];
+
+            rateElement.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+            amountElement_two.value = (amountElement_one.value * rate).toFixed(2);
+        });    
+}
+
+// Event listeners
+currencyElement_one.addEventListener('change', calculate);
+amountElement_one.addEventListener('input', calculate);
+currencyElement_two.addEventListener('change', calculate);
+amountElement_two.addEventListener('input', calculate);
+
+
+calculate();
