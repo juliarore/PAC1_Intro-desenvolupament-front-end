1170adbffc54e48d35b928462baea9882b689ef0
Júlia Romero <juliarore6@gmail.com>
2025-10-10 16:28:55 +0200

    feat: populate UI with selected movie and seats from local storage

--- PAC1_Ex2_3/script.js
+++ PAC1_Ex2_3/script.js
@@ -4,7 +4,10 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
 
 // Save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
@@ -16,14 +19,33 @@ function setMovieData(movieIndex, moviePrice) {
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+    // Save selected seats in local storage
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
+    // Display selected seats and total price
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice; 
+}    
+
+// Get data from local storage and populate UI
+function populateUI() {
+    // Get selected seats from local storage
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.includes(index)) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+    // Get selected movie index from local storage
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }    
 
 // Movie select event
@@ -43,3 +65,5 @@ container.addEventListener('click', (e) => {
     }
 });    
 
+// Initial count and total set
+updateSelectedCount();
